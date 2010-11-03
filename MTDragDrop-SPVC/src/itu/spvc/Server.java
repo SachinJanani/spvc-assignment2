50800aee9d9c2f21b2d88a41a66d74986272e9ed
konrad.bajtyngier <konrad.bajtyngier@gmail.com>
2010-11-03 11:51:26 +0000

    all surface

--- MTDragDrop-SPVC/src/itu/spvc/Server.java
+++ MTDragDrop-SPVC/src/itu/spvc/Server.java
@@ -3,6 +3,7 @@ package itu.spvc;
 import java.awt.Image;
 import java.awt.Toolkit;
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -10,15 +11,18 @@ import java.net.Socket;
 public class Server extends Thread {
 	ServerSocket serverSocket = null;
 	Socket sock = null;
-	
-//	ObjectOutputStream out;	
+
+	// ObjectOutputStream out;
 	DataInputStream inputStream;
-	
+	DataOutputStream out;
+
 	DragDropScene scene;
 
 	/**
-	 *  Create the server and start the thread listening
-	 * @param ddscene the scene that will receive image and events
+	 * Create the server and start the thread listening
+	 * 
+	 * @param ddscene
+	 *            the scene that will receive image and events
 	 */
 	public Server(DragDropScene ddscene) {
 		super("socketListener");
@@ -26,84 +30,89 @@ public class Server extends Thread {
 		this.start();
 	}
 
-	public void run () {
+	public void run() {
 		try {
-			// we open the connection and 
+			// we open the connection and
 			// get ready to listen to incoming information
 			serverSocket = new ServerSocket(50543);
 			sock = serverSocket.accept();
 			inputStream = new DataInputStream(sock.getInputStream());
+			out = new DataOutputStream(sock.getOutputStream());
 			System.out.println("Accepted connection : " + sock);
 
-			//sendMessage("Connection successful");
+			sendMessage("Connection successful");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
 		try {
 			while (true) {
-				//System.out.println("Waiting...");
+				// System.out.println("Waiting...");
 
 				// listening for messages
-				if (inputStream.available() >0) {
+				if (inputStream.available() > 0) {
 					byte head = inputStream.readByte();
 					System.out.println(head);
-					// We check the message header 
+					// We check the message header
 					// 1 means image coming
 					// 2 means coordinates
 					// anything else means stop communication
-					if (head==1) {
-						// We get the size of the image, 
+					if (head == 1) {
+						// We get the size of the image,
 						// to know how much we have to read from the socket
 						int size = inputStream.readInt();
 						System.out.println(size);
 						byte[] imgBytes = new byte[size];
 						int soFar = 0;
 
-						// "read" doesn't make sure that all the "size" bytes are read!
-						// So while we did not get the full image we keep reading.
-						while(soFar < size) {
-							int readNow = inputStream.read(imgBytes, soFar, (size-soFar));
-							if(readNow > 0) {
+						// "read" doesn't make sure that all the "size" bytes
+						// are read!
+						// So while we did not get the full image we keep
+						// reading.
+						while (soFar < size) {
+							int readNow = inputStream.read(imgBytes, soFar,
+									(size - soFar));
+							if (readNow > 0) {
 								soFar += readNow;
 							}
-							System.out.println("read " + readNow + ", missing: " + (size-soFar));
+							System.out.println("read " + readNow
+									+ ", missing: " + (size - soFar));
 						}
-//						System.out.println(inputStream.read(imgBytes, 0, size));
+						// System.out.println(inputStream.read(imgBytes, 0,
+						// size));
 
-						// Once we got the all the byte of the image we rebuild it.
-						Image awtImage = Toolkit.getDefaultToolkit().createImage(imgBytes);
+						// Once we got the all the byte of the image we rebuild
+						// it.
+						Image awtImage = Toolkit.getDefaultToolkit()
+								.createImage(imgBytes);
 						// And add it to the scene
 						scene.addImage(awtImage);
-					} 
-					else if (head==2) {
+					} else if (head == 2) {
+						// TODO:
 						// We get x and y coordinates from the move commands
-						// We move the latest image added to the server 
+						// We move the latest image added to the server
 						// scene.moveImage(x,y);
-					} 
-					else break;
+					} else
+						break;
 				}
 			}
 			System.out.println("Closing");
 
-//			out.close();
+			// out.close();
 			serverSocket.close();
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	/*
+
 	void sendMessage(String msg) {
-		try{
-			out.writeObject(msg);
+		try {
+			out.writeBytes(msg);
 			out.flush();
 			System.out.println("server>" + msg);
-		}
-		catch(IOException ioException){
+		} catch (IOException ioException) {
 			ioException.printStackTrace();
 		}
 	}
-	*/
 }
