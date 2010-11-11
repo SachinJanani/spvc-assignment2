e01eb0fd85a71fb39e5a58a5de1a8f8f7754247e
konrad.bajtyngier <konrad.bajtyngier@gmail.com>
2010-11-11 14:06:21 +0000

    sad

---
+++ SPVCDisplayLocation/src/dk/itu/spvc/android/bt/BlipLocationDisplayActivity.java
@@ -0,0 +1,131 @@
+package dk.itu.spvc.android.bt;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import dk.itu.android.bluetooth.BluetoothAdapter;
+import dk.itu.android.bluetooth.BluetoothDevice;
+
+public class BlipLocationDisplayActivity extends Activity {
+
+	/* used when requesting the bluetooth-enabling activity */
+	static final int REQUEST_BLUETOOTH_ENABLE = 1;
+	/* reference to the button used to start the discovery phase */
+	Button startDiscoveryButton;
+	/* the local bluetooth adapter */
+	BluetoothAdapter btadapter;
+	/* view adapter for the list of devices */
+	ArrayAdapter<String> devicesArrayAdapter;
+
+	public void startDiscovery(View view) {
+		if (btadapter.isDiscovering()) {
+			btadapter.cancelDiscovery();
+		}
+		btadapter.startDiscovery();
+		startDiscoveryButton.setEnabled(false);
+	}
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		dk.itu.android.bluetooth.BluetoothAdapter.SetContext(this);
+		/* create the list view adapter */
+		devicesArrayAdapter = new ArrayAdapter<String>(this,
+				R.layout.device_name);
+		/* get a reference to the list */
+		ListView devicesListView = (ListView) findViewById(R.id.DevicesListView);
+		/* set the adapter */
+		devicesListView.setAdapter(devicesArrayAdapter);
+		/* get a reference to the StartDiscoveryButton */
+		startDiscoveryButton = (Button) findViewById(R.id.StartDiscoveryButton);
+	}
+
+	BroadcastReceiver discoveryReceiver = new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			/* do something with the intent here */
+			String action = intent.getAction();
+			if (action.equals(BluetoothDevice.ACTION_FOUND)) {
+				BluetoothDevice device = intent
+						.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+				if (device.getBondState() != BluetoothDevice.BOND_BONDED) {
+					devicesArrayAdapter.add(device.getName() + "\n"
+							+ device.getAddress());
+				}
+				Log.v("KONRAD", "Device name: " + device.getName());
+				String name = device.getName();
+				TextView locationTextView = (TextView) findViewById(R.id.LocationTextView);
+				if (name != null && name.matches("ITU-.*")) {
+					locationTextView.setText(name);
+				} else {
+					locationTextView.setText("unknown");
+				}
+			} else if (action
+					.equals(BluetoothAdapter.ACTION_DISCOVERY_FINISHED)) {
+				startDiscoveryButton.setEnabled(true);
+			}
+		}
+	};
+
+	public void onStart() {
+		super.onStart();
+		btadapter = BluetoothAdapter.getDefaultAdapter();
+		if (btadapter == null) {
+			/* uh-oh.. no bluetooth module found! */
+			Toast.makeText(this, "Sorry, no bluetooth module found!",
+					Toast.LENGTH_SHORT);
+			/* terminate the activity */
+			finish();
+		} else {
+			if (!btadapter.isEnabled()) {
+				startActivityForResult(new Intent(
+						BluetoothAdapter.ACTION_REQUEST_ENABLE),
+						REQUEST_BLUETOOTH_ENABLE);
+			} else {
+				setup();
+			}
+		}
+	}
+
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (REQUEST_BLUETOOTH_ENABLE == requestCode) {
+			if (RESULT_OK == resultCode) {
+				setup();
+			} else {
+				Toast.makeText(this,
+						"Cannot do anything if bluetooth is disabled :(",
+						Toast.LENGTH_SHORT);
+				finish();
+			}
+
+		}
+	}
+
+	private void setup() {
+		for (BluetoothDevice device : btadapter.getBondedDevices()) {
+			devicesArrayAdapter.add(device.getName() + "\n"
+					+ device.getAddress());
+		}
+		IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+		registerReceiver(discoveryReceiver, filter);
+		filter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+		registerReceiver(discoveryReceiver, filter);
+	}
+
+	public void onDestroy() {
+		btadapter.cancelDiscovery();
+		unregisterReceiver(discoveryReceiver);
+	}
+}
