33810adf3f4979cabcf3c37e6778081e261f88e4
edevantie <edevantie@f6870b21-4098-94a5-9758-db9a9e3af258>
2010-11-12 10:49:28 +0000

    Commit #5

--- trunk/SPVCBlipLocationShare/src/dk/itu/spvc/bliploc/Threads.java
+++ trunk/SPVCBlipLocationShare/src/dk/itu/spvc/bliploc/Threads.java
@@ -1,41 +1,15 @@
 package dk.itu.spvc.bliploc;
 
+import android.bluetooth.BluetoothClass.Device;
+import android.content.BroadcastReceiver;
+
 public class Threads extends Thread {
 	
-	Thread t = new Thread();
-	
-	int count;
-	  Threads() {
-	    count = 0;
-	  }
-	  
 	  public void run() {
-	    System.out.println("MyThread starting.");
-	    try {
-	      do {
-	        Thread.sleep(30000);
-	        System.out.println("In MyThread, count is " + count);
-	        count++;
-	      } while (count < 5);
-	    } catch (InterruptedException exc) {
-	      System.out.println("MyThread interrupted.");
-	    }
-	    System.out.println("MyThread terminating.");
-	  
+		    for ( int i = 0; i < 10; i++ ) {
+	    		
+		    }
+		  }
 	
-	
-	
-
-	    
-	  	    do {
-	      System.out.println("In main thread.");
-	      try {
-	        Thread.sleep(250);
-	      } catch (InterruptedException exc) {
-	        System.out.println("Main thread interrupted.");
-	      }
-	    } while (t.count != 5);
-	    System.out.println("Main thread ending.");
-	  }
  }
 
